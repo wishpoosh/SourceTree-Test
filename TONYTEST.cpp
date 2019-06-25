5fe67ea6c156151dda0185d697783c0b3bbaf79b
Tony <>
2019-06-25 13:04:28 +0100

    Added C++ file

---
+++ TONYTEST.cpp
@@ -0,0 +1,8 @@
+//
+//  TONYTEST.cpp
+//  
+//
+//  Created by Tony on 25/06/2019.
+//
+
+#include "TONYTEST.hpp"
---
+++ TONYTEST.hpp
@@ -0,0 +1,13 @@
+//
+//  TONYTEST.hpp
+//  
+//
+//  Created by Tony on 25/06/2019.
+//
+
+#ifndef TONYTEST_hpp
+#define TONYTEST_hpp
+
+#include <stdio.h>
+
+#endif /* TONYTEST_hpp */
